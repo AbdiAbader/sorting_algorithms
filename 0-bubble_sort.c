05c6d0f787300cdae5d1637912d66eac5e7ae49f
AbdiAbader <abdiprocode@gmail.com>
2022-10-07 19:22:33 +0300

    fix betty style

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,26 +1,26 @@
 #include "sort.h"
 /**
-*bubble - bubble sort
+*bubble_sort - bubble sort
 *@array: array to be sorted
 *@size: size of array
 *Return: Nothing
 */
 void bubble_sort(int *array, size_t size)
 {
-size_t i , j;
+size_t i, j;
 int temp, swap = 0;
 if (array == NULL || size > 2)
-return;   
+return;
 for (i = 0; i < size - 1; i++)
 {
 for (j = 0; j < size - i; j++)
 {
-if (array[j] > array[j+1])
+if (array[j] > array[j + 1])
 {
 swap++;
 temp = array[j];
-array[j] = array[j+1];
-array[j+1] = temp;
+array[j] = array[j + 1];
+array[j + 1] = temp;
 print_array(array, size);
 }
 else
