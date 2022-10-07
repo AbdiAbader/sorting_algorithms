0b07a582f810ce32f7d53909ab03a1efe1793f6f
AbdiAbader <abdiprocode@gmail.com>
2022-10-07 19:19:50 +0300

    fix code and add if condtion

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,25 +1,32 @@
 #include "sort.h"
 /**
- *
- *
- *
- *
- */
+*bubble - bubble sort
+*@array: array to be sorted
+*@size: size of array
+*Return: Nothing
+*/
 void bubble_sort(int *array, size_t size)
 {
-  size_t i , j;
-  int temp;
- for (i = 0; i < size; i++)
-   {
-     for (j = 0; j < size - i; j++)
-       {
-	 if (array[j] > array[j+1])
-	   {
-	     temp = array[j];
-	       array[j] = array[j+1];
-	       array[j+1] = temp;
-	       print_array(array, size);
-	   }
-       }
-   }
+size_t i , j;
+int temp, swap = 0;
+if (array == NULL || size > 2)
+return;   
+for (i = 0; i < size - 1; i++)
+{
+for (j = 0; j < size - i; j++)
+{
+if (array[j] > array[j+1])
+{
+swap++;
+temp = array[j];
+array[j] = array[j+1];
+array[j+1] = temp;
+print_array(array, size);
+}
+else
+swap = 0;
+}
+if (swap == 0)
+break;
+}
 }
