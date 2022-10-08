a799286365fa770a215276ae47b65a3c8b16a93f
AbdiAbader <abdiprocode@gmail.com>
2022-10-08 06:22:13 +0300

    fix code on if condition

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,11 +9,11 @@ void bubble_sort(int *array, size_t size)
 {
 size_t i, j;
 int temp, swap = 0;
-if (array == NULL || size > 2)
+if (array == NULL || size < 2)
 return;
 for (i = 0; i < size; i++)
 {
-  swap = 0;
+swap = 0;
 for (j = 0; j < size - i - 1; j++)
 {
 if (array[j] > array[j + 1])
