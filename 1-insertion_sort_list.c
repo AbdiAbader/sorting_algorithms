67bb93b12a8bd80d70dac5bff369f55274cf0c03
AbdiAbader <abdiprocode@gmail.com>
2022-10-09 10:47:57 +0300

    fix and modify code

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,35 +1,29 @@
 #include "sort.h"
 /**
-* insertion_sort_list - sorts a doubly linked list of integers in ascending
-* order using the Insertion sort algorithm
-* @list: Double pointer to the head of the linked list
-*
-* Return: void
+*insertion_sort_list - implments insertion in linked list
+*@list: list of double linked list
+*Return: void
 */
 void insertion_sort_list(listint_t **list)
 {
-listint_t *swap_node, *next_swap;
-
+listint_t  *tmp, *hold;
 if (list == NULL || *list == NULL)
-  return;
-swap_node = (*list)->next;
-while (swap_node != NULL)
+return;
+tmp = (*list)->next;
+while (tmp)
 {
-  next_swap = swap_node->next;
-  while (swap_node->prev != NULL && swap_node->prev->n > swap_node->n)
-  {
-	  swap_node->prev->next = swap_node->next;
-	  if (swap_node->next != NULL)
-		  swap_node->next->prev = swap_node->prev;
-	  swap_node->next = swap_node->prev;
-	  swap_node->prev = swap_node->next->prev;
-	  swap_node->next->prev = swap_node;
-	  if (swap_node->prev == NULL)
-		  *list = swap_node;
-	  else
-		  swap_node->prev->next = swap_node;
-	  print_list(*list);
-  }
-  swap_node = next_swap;
+hold = tmp->next;
+while (tmp->prev && tmp->prev->n > tmp->n)
+{
+tmp->prev->next = tmp->next;
+if (tmp->next)
+tmp->next->prev = tmp->prev;
+tmp->next = tmp->prev;
+tmp->prev = tmp->next->prev;
+tmp->next->prev = tmp;
+tmp->prev == NULL ? (*list = tmp) : (tmp->prev->next = tmp);
+print_list(*list);
+}
+tmp = hold;
 }
 }
